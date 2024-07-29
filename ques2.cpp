27d9626276bad3d6b50fd0506b83549c794ff831
shourya-mukhi <22103323@mail.jiit.ac.in>
2024-07-29 15:48:21 +0530

    Add files via upload

---
+++ ques2.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d integers:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+
+    float average = (float)sum / n;
+    printf("The average of the integers is: %.2f\n", average);
+
+    return 0;
+}
